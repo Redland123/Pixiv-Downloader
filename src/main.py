e05dba0212c2bcd738db9cbbf76bc2a99925751d
Redland123 <48281271+Redland123@users.noreply.github.com>
2020-04-21 19:57:42 -0700

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,103 @@
+#https://pixiv-api.readthedocs.io/en/latest/
+#$ pip install pixiv-api 
+
+from pathlib import Path
+from pixivapi import Client
+from pixivapi import Size
+from pixivapi import Visibility
+from os import system
+
+#print("Hello world")
+
+def downloadSingleImg(currentClient):
+    if not currentClient:
+        return -1
+
+    url = input('Enter img URL: ')
+    fName = input('Enter file name: ')
+    imgLoc = 'images/'
+
+    if not url or not imgLoc or not fName:
+        return -1
+
+    newImg = currentClient.fetch_illustration(
+        url.split('https://www.pixiv.net/en/artworks/')[1])
+
+    newImg.download(directory=Path(imgLoc), size=Size.ORIGINAL, filename=fName)
+    print ('Image downloaded')
+
+    return 1
+
+def downloadBooksmarks(currentClient, userId, check, amount):
+    bkMarks = currentClient.fetch_user_bookmarks(user_id=userId, visibility=Visibility.PUBLIC, max_bookmark_id=None, tag=None)
+
+    imgLoc = 'images/'
+
+    if check:
+        while True:
+            for currentImg in bkMarks['illustrations']:
+                try:
+                    currentImg.download(directory=Path(imgLoc), size=Size.ORIGINAL)
+                    print ('Image downloaded')
+                except:
+                    print ('Error downloading?')
+
+            # Breaks the loop if a next page does not exist
+            if not bkMarks['next']:
+                break
+
+            # Moves the list to the next page
+            bkMarks = currentClient.fetch_user_bookmarks(userId, max_bookmark_id=bkMarks['next'])
+
+    if not check:
+        for i in range(int(amount)):
+            try:
+                bkMarks['illustrations'][i].download(directory=Path(imgLoc), size=Size.ORIGINAL)
+                print ('Image downloaded')
+            except:
+                print ('Error downloading?')
+
+
+def main():
+    #Creates new session
+    currentSession = Client()
+
+    userName = input ('Enter email: ') 
+    passWord = input ('Enter password: ')
+
+    #Authenticates the current session
+    try:
+        currentSession.login(userName, passWord)
+    except:
+        print('Error logging into account.')
+        return -1
+
+    _ = system('cls')
+    print ('\nSlect an option: ')
+    print ('0: exit.')
+    print ('1: Download a single image.')
+    print ('2: Download a range of bookmarks. (Current max = 30, need to fix.)')
+    print ('3: Download all booksmarks.')
+
+    _ = system('cls')
+
+    ans = input()
+
+    if ans == '0':
+        return 0
+    
+    if ans == '1':
+        downloadSingleImg(currentSession)
+
+    if ans == '2':
+        userId = input('Enter userId: ')
+        num = input('Enter the number of images to be downloaded: ')
+        downloadBooksmarks(currentSession, userId, 0, num)
+
+    if ans == '3':
+        userId = input('Enter userId: ')
+        downloadBooksmarks(currentSession, userId, 1, 0)
+
+
+if __name__ == '__main__':
+    main()
